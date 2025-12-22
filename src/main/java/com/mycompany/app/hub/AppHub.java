2f8ccd6ea60c835f6046557d84cb3f499420aa03
nathsuki73 <nathsegovia73@gmail.com>
2025-12-22 15:07:27 +0800

    Initial Commit

---
+++ src/main/java/com/mycompany/app/hub/AppHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app.hub;
+
+/**
+ *
+ * @author Tan
+ */
+public class AppHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
