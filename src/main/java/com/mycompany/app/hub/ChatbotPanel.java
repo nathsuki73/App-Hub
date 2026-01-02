272b5ff9d1f07d97209a91d13987bdb212ccd68d
Tan <nathsegovia73@gmail.com>
2026-01-02 17:44:16 +0800

    updated ChatBot Panel for textbox

--- src/main/java/com/mycompany/app/hub/ChatbotPanel.java
+++ src/main/java/com/mycompany/app/hub/ChatbotPanel.java
@@ -14,10 +14,13 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -34,6 +37,8 @@ public class ChatbotPanel extends javax.swing.JPanel {
     public ChatbotPanel() {
         initComponents();
         jScrollPane1.getVerticalScrollBar().setUnitIncrement(5);
+        initCustomFont();
+        
         CreateMessage();
         CreateMessage();
         CreateMessage();
@@ -41,6 +46,130 @@ public class ChatbotPanel extends javax.swing.JPanel {
         CreateMessage();
     }
 
+    
+     class RoundedButton extends JButton {
+        private int radius;
+        private Color normalColor = new Color (84,16,103);
+        private Color hoverColor = Color.WHITE;
+
+        private Color originalTextColor;
+        
+        public RoundedButton(String text, int radius)
+        {
+            super(text);
+            this.radius = radius;
+            this.originalTextColor = Color.WHITE;
+            
+            setContentAreaFilled(false);
+            setFocusPainted(false);
+            setBorderPainted(false);
+            setOpaque(false);
+            
+            setBackground(normalColor);
+            setForeground(originalTextColor);
+            
+            addMouseListener(new MouseAdapter() 
+            {
+                @Override
+                public void mouseEntered(MouseEvent e)
+                {
+                    if (!isEnabled()) return;
+                    setBackground(hoverColor);
+                }
+                
+                @Override
+                public void mouseExited(MouseEvent e)
+                {
+                    if (!isEnabled()) return;
+                    setBackground(normalColor);
+                }
+            });
+        }
+        
+        public RoundedButton(String text, int radius, Color color)
+        {
+            super(text);
+            normalColor = color;
+            this.radius = radius;
+            this.originalTextColor = Color.WHITE;
+            
+            setContentAreaFilled(false);
+            setFocusPainted(false);
+            setBorderPainted(false);
+            setOpaque(false);
+            
+            setBackground(normalColor);
+            setForeground(originalTextColor);
+            
+            addMouseListener(new MouseAdapter() 
+            {
+                @Override
+                public void mouseEntered(MouseEvent e)
+                {
+                    if (!isEnabled()) return;
+                    setBackground(hoverColor);
+                }
+                
+                @Override
+                public void mouseExited(MouseEvent e)
+                {
+                    if (!isEnabled()) return;
+                    setBackground(normalColor);
+                }
+            });
+        }
+        
+        @Override
+        protected void paintComponent(Graphics g)
+        {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            
+            if (!isEnabled())
+            {
+                g2.setColor(Color.LIGHT_GRAY);
+            }
+            else
+            {
+                g2.setColor(getBackground());
+            }
+            //g2.setColor(getBackground());
+            g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+            
+            g2.dispose();
+            
+            super.paintComponent(g);
+        }
+    }
+     
+     
+    public void initCustomFont() {
+    try {
+        InputStream is = getClass().getResourceAsStream("/fonts/inter.ttf"); 
+        if (is == null) {
+            throw new IOException("Font file not found in resources folder!");
+        }
+
+        Color customPurple = new Color(132,81,138);
+        Font myFont = Font.createFont(Font.TRUETYPE_FONT, is);
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(myFont);
+
+        txtSearchText.setFont(myFont.deriveFont(Font.PLAIN ,16f));
+        txtSearchText.setForeground(new java.awt.Color(150, 150, 150));
+        txtSearchText.setText("Ask anything...");
+        
+        
+        
+    } catch (FontFormatException e) {
+        System.err.println("The font file is corrupted or not a valid TTF.");
+        e.printStackTrace();
+    } catch (IOException e) {
+        System.err.println("Could not load the font file. Check the path!");
+        e.printStackTrace();
+    }
+}
     
     private void CreateMessage() {
         
@@ -160,12 +289,30 @@ public class ChatbotPanel extends javax.swing.JPanel {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
+        jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         messageContainer = new javax.swing.JPanel();
+        jPanel1 = new RoundedPanel(40);
+        txtSearchText = new javax.swing.JTextField();
+        jButton1 = new RoundedButton("", 40);
 
         setBackground(new java.awt.Color(52, 8, 69));
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                formMouseClicked(evt);
+            }
+        });
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                formKeyPressed(evt);
+            }
+        });
         setLayout(new java.awt.GridBagLayout());
+
+        jPanel2.setBackground(new java.awt.Color(52, 8, 69));
+        jPanel2.setLayout(new java.awt.GridBagLayout());
 
         jScrollPane1.setBackground(new java.awt.Color(52, 8, 69));
         jScrollPane1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
@@ -177,15 +324,87 @@ public class ChatbotPanel extends javax.swing.JPanel {
         messageContainer.setBackground(new java.awt.Color(52, 8, 69));
         messageContainer.setMaximumSize(new java.awt.Dimension(100, 100));
         messageContainer.setMinimumSize(new java.awt.Dimension(100, 100));
+        messageContainer.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                messageContainerMouseClicked(evt);
+            }
+        });
         messageContainer.setLayout(new javax.swing.BoxLayout(messageContainer, javax.swing.BoxLayout.Y_AXIS));
         jScrollPane1.setViewportView(messageContainer);
 
-        add(jScrollPane1, new java.awt.GridBagConstraints());
+        jPanel2.add(jScrollPane1, new java.awt.GridBagConstraints());
+
+        jPanel1.setBackground(new java.awt.Color(84, 16, 103));
+        jPanel1.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        jPanel1.setPreferredSize(new java.awt.Dimension(600, 60));
+        jPanel1.setLayout(new java.awt.BorderLayout(20, 0));
+
+        txtSearchText.setBackground(new java.awt.Color(84, 16, 103));
+        txtSearchText.setForeground(new java.awt.Color(255, 255, 255));
+        txtSearchText.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        txtSearchText.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtSearchTextFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txtSearchTextFocusLost(evt);
+            }
+        });
+        jPanel1.add(txtSearchText, java.awt.BorderLayout.CENTER);
+
+        jButton1.setBackground(new java.awt.Color(84, 16, 103));
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Group 14.png"))); // NOI18N
+        jButton1.setBorder(null);
+        jButton1.setBorderPainted(false);
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.setFocusPainted(false);
+        jButton1.setPreferredSize(new java.awt.Dimension(40, 40));
+        jPanel1.add(jButton1, java.awt.BorderLayout.EAST);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.insets = new java.awt.Insets(26, 0, 0, 0);
+        jPanel2.add(jPanel1, gridBagConstraints);
+
+        add(jPanel2, new java.awt.GridBagConstraints());
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_formKeyPressed
+
+    private void formMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseClicked
+        this.requestFocusInWindow();
+    }//GEN-LAST:event_formMouseClicked
+
+    private void txtSearchTextFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtSearchTextFocusLost
+        if (txtSearchText.getText().isEmpty()) {
+            // Change back to a dim color for the placeholder
+            txtSearchText.setForeground(new java.awt.Color(150, 150, 150));
+            txtSearchText.setText("Ask anything...");
+        }
+    }//GEN-LAST:event_txtSearchTextFocusLost
+
+    private void txtSearchTextFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtSearchTextFocusGained
+        if (txtSearchText.getText().equals("Ask anything...")) {
+            txtSearchText.setText("");
+            // Change to a bright color for actual typing
+            txtSearchText.setForeground(java.awt.Color.WHITE);
+        }
+    }//GEN-LAST:event_txtSearchTextFocusGained
+
+    private void messageContainerMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_messageContainerMouseClicked
+                this.requestFocusInWindow();
+
+    }//GEN-LAST:event_messageContainerMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPanel messageContainer;
+    private javax.swing.JTextField txtSearchText;
     // End of variables declaration//GEN-END:variables
 }
